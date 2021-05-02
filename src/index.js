827ff3ffb10fe825274f9512da716286e35999a1
PaperOrb <48017994+PaperOrb@users.noreply.github.com>
2021-05-02 17:29:02 -0400

    GUI toggles temp scale display for search results

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const searchForm = document.querySelector(".city-search");
 const searchField = document.querySelector(".city-search__input");
 const cardTemplate = document.querySelector("#template-weather-card");
 const cardContainer = document.querySelector(".weather-card-container");
+const tempToggle = document.querySelector(".check");
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -16,4 +17,8 @@ searchForm.addEventListener("submit", (e) => {
     let cardEle = weather.createCard(data, cardTemplate);
     weather.renderCard(cardEle, cardContainer);
   });
+});
+
+tempToggle.addEventListener("click", (e) => {
+  weather.toggleTempScale();
 });
--- src/modules/weather.js
+++ src/modules/weather.js
@@ -1,6 +1,15 @@
 export { weather };
 
 const weather = (function () {
+  let tempScale = convertToFahrenheit;
+
+  function getTempScale() {
+    return tempScale;
+  }
+  function setTempScale(scale) {
+    tempScale = scale;
+  }
+
   async function fetchData(searchTerm) {
     try {
       let response = await fetch(
@@ -22,7 +31,7 @@ const weather = (function () {
     let clone = template.cloneNode(true);
     clone.setAttribute("style", "display: flex;");
     clone.querySelector(".city").textContent = data.name;
-    clone.querySelector(".temperature").textContent = `${convertToFahrenheit(data.main.temp)} F`;
+    clone.querySelector(".temperature").textContent = `${convertTemp(data.main.temp)}`;
     clone.querySelector(".weather-art").setAttribute("src", `images/${data.weather[0].icon}.png`);
     clone.querySelector(".humidity").textContent = `Humidity: ${data.main.humidity}%`;
     clone.querySelector(".cloudiness").textContent = `Cloudiness: ${data.clouds.all}%`;
@@ -31,21 +40,33 @@ const weather = (function () {
     return clone;
   }
 
+  function convertTemp(kelvin) {
+    let scale = getTempScale();
+    debugger;
+    return scale(kelvin);
+  }
+
+  function toggleTempScale() {
+    let scale = getTempScale() === convertToFahrenheit ? convertToCelsius : convertToFahrenheit;
+    setTempScale(scale);
+    debugger;
+  }
+
   function convertMeterToMile(meters) {
     return (meters * 3600 * 0.00062137).toFixed();
   }
 
   function convertToFahrenheit(kelvin) {
-    return ((kelvin - 273.15) * 1.8 + 32).toFixed();
+    return `${((kelvin - 273.15) * 1.8 + 32).toFixed()} F`;
   }
 
   function convertToCelsius(kelvin) {
-    return (kelvin - 273.15).toFixed();
+    return `${(kelvin - 273.15).toFixed()} C`;
   }
 
   function renderCard(cardEle, cardContainer) {
     cardContainer.append(cardEle);
   }
 
-  return { createCard, fetchData, renderCard, cityName };
+  return { createCard, fetchData, renderCard, cityName, toggleTempScale };
 })();
